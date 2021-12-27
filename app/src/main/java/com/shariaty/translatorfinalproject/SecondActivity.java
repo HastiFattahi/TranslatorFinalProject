bf5649e0219194de507ec0d12361713bd52fc4ec
Hasti <hastifattahii21@gmail.com>
2021-12-27 19:27:57 +0330

    Main activity UI

--- app/src/main/java/com/shariaty/translatorfinalproject/SecondActivity.java
+++ app/src/main/java/com/shariaty/translatorfinalproject/SecondActivity.java
@@ -3,12 +3,43 @@ package com.shariaty.translatorfinalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
 
+public class SecondActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
+    private Spinner spinner2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        
+        spinner=findViewById(R.id.spinner1);
+        spinner2=findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence>spinneradapter=ArrayAdapter.createFromResource(this,R.array.Lang_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence>spinneradapter2=ArrayAdapter.createFromResource(this,R.array.Lang_array, android.R.layout.simple_spinner_item);
+        spinneradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinneradapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinneradapter);
+        spinner2.setAdapter(spinneradapter2);
+
+        spinner.setOnItemSelectedListener(this);
+        spinner2.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getApplicationContext(),choice,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
---
+++ app/src/main/java/com/shariaty/translatorfinalproject/translation.java
@@ -0,0 +1,14 @@
+package com.shariaty.translatorfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class translation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translation);
+    }
+}
